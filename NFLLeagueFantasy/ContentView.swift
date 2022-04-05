92c6db386af8841d5d0c69cd931fce8045a26f41
Kevin Stradtman <webkit.development.systems@gmail.com>
2022-04-04 22:50:24 -0400

    Initial Commit

---
+++ NFLLeagueFantasy/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NFLLeagueFantasy
+//
+//  Created by Kevin Stradtman on 4/4/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NFLLeagueFantasy/NFLLeagueFantasyApp.swift
@@ -0,0 +1,17 @@
+//
+//  NFLLeagueFantasyApp.swift
+//  NFLLeagueFantasy
+//
+//  Created by Kevin Stradtman on 4/4/22.
+//
+
+import SwiftUI
+
+@main
+struct NFLLeagueFantasyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
